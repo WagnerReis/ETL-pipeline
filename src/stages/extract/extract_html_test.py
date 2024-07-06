1bb865a26d7e85b173ff2f9ced91b04ce6b8ffe7
WagnerReis <wagnerreis1997@gmail.com>
2024-07-06 19:16:30 -0300

    feat: add contracts and error class for extraction

--- src/stages/extract/extract_html_test.py
+++ src/stages/extract/extract_html_test.py
@@ -1,5 +1,7 @@
 from src.drivers.http_requester import HttpRequester
 from src.drivers.html_collector import HtmlCollector
+from src.stages.contracts.extract_contract import ExtractContract
+from src.errors.extract_error import ExtractError
 
 from .extract_html import ExtractHtml
 
@@ -12,3 +14,17 @@ def test_extract():
     response = extract_html.extract()
     print()
     print(response)
+
+    assert isinstance(response, ExtractContract)
+
+
+def test_extract_error():
+    http_requester = 'any'
+    html_collector = HtmlCollector()
+
+    extract_html = ExtractHtml(http_requester, html_collector)
+
+    try:
+        extract_html.extract()
+    except Exception as exception:  # pylint: disable=broad-exception-caught
+        assert isinstance(exception, ExtractError)
